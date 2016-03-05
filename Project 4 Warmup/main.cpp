faeba998b1f7c63e903b9e98025fe8a8bba1a787
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-03-04 19:51:18 -0800

    Initial Commit

---
+++ Project 4 Warmup/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 4 Warmup
+//
+//  Created by Gopi Suresh on 3/4/16.
+//  Copyright © 2016 Gopi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
